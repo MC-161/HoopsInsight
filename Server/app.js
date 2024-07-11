3cff692c55f28ed7f64f881a59930eace86631a5
Monkey-D-MC1 <mahfuzc542@gmail.com>
2024-07-11 19:11:41 +0100

    prep for docs

--- Server/app.js
+++ Server/app.js
@@ -1,14 +1,27 @@
 import bodyParser from 'body-parser';
 import routes from './routes/index.js';
 import createServer from './server.js';
+import cors from 'cors'
+import rateLimit from 'express-rate-limit';
+
 
 const app = createServer();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 // Middleware to parse incoming request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cors())
 app.use('/', routes);
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 // Middleware to handle routes
 // Error handling middleware
